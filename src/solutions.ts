11751cb017243807a593b31f0965af19d192fe14
ehasun15019 <ehasunulislam0202@gmail.com>
2026-05-06 21:38:16 +0600

    solution q-2

--- src/solutions.ts
+++ src/solutions.ts
@@ -7,4 +7,13 @@ function filterEvenNumbers(nums: number[]): number[] {
 
 const result1 = filterEvenNumbers([1, 2, 3, 4, 5, 6]);
 
-console.log(result1);
+// console.log(result1);
+
+
+// question solved - 2
+function reverseString(str : string) {
+    return str.split("").reverse().join("")
+}
+
+const result2 = reverseString("typescript");
+// console.log(result2);
