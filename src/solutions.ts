a7f432350b5ffb9b4316b131474cf2bee53755f1
ehasun15019 <ehasunulislam0202@gmail.com>
2026-05-06 21:21:25 +0600

    solution q-1

--- src/solutions.ts
+++ src/solutions.ts
@@ -1,1 +1,10 @@
 // question solved - 1 
+function filterEvenNumbers(nums: number[]): number[] {
+    return nums.filter(num => {
+       return num % 2 === 0
+    })
+}
+
+const result1 = filterEvenNumbers([1, 2, 3, 4, 5, 6]);
+
+console.log(result1);
